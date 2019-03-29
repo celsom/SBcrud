59773570deede72e36a4ca2a6bc698aab47f0f39
MuhammadWarid <waridfbw1997@gmail.com>
2019-03-29 07:50:28 +0200

    used bootsrap and added another page

---
+++ filtered.php
@@ -0,0 +1,69 @@
+<?php include('server.php') ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <title>Standard Bank</title>
+  </head>
+  <body >
+      <div class="row">
+        <div class="col-sm-3 col-md-3 col-lg-3">
+            <img src="img/img1.jpg" class="img-responsive" alt="standard bank">
+        </div>
+      <div id="content-1" class="col-sm-8 col-md-8 col-lg-8 offset-sm-1">
+        <h4 id="special-h4">Veja as <strong>máquinas</strong> que encontramos para si</h4>
+        <hr>
+        <div class="container">
+          <div class="row">
+          <div class="col-sm-4 col-md-4 col-lg-4 ">
+            <img src="img/BMW" class="logos">
+            <p id="logo-caption">BMW 525i - Leasing</p>
+          </div>
+          <div class="col-sm-4 col-md-4 col-lg-4 ">
+            <img src="img/HYUNDAI" class="logos">
+            <p id="logo-caption">Hyundai i35 - Leasing</p>
+          </div>
+          <div class="col-sm-4 col-md-4 col-lg-4 ">
+            <img src="img/TOYOTA" class="logos">
+            <p id="logo-caption">Toyota Alteza - Leasing</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-4 col-md-4 col-lg-4 ">
+            <img src="img/FORD" class="logos">
+            <p id="logo-caption">Ford F-350</p>
+          </div>
+          <div class="col-sm-4 col-md-4 col-lg-4 ">
+            <img src="img/TOYOTA" class="logos">
+            <p id="logo-caption">Toyota Fortuner 2019</p>
+          </div>
+          <div class="col-sm-4 col-md-4 col-lg-4 ">
+            <img src="img/BMW" class="logos">
+            <p id="logo-caption">BMW 7 series 2019</p>
+          </div>
+        </div>
+
+        </div>
+        <hr>
+        <p>Todos direitos reservados. Standard Bank SA, 2019</p>
+      </div>
+
+</div>
+  
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
